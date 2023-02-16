640b9c440b0883acedc1088e5a2763e015e2f9a5
Guilherme S. Zulian <guilhermesz873@gmail.com>
2023-02-16 03:04:29 -0300

    feat: melhorado readme e makefile

--- test/Cases/ClientTest.php
+++ test/Cases/ClientTest.php
@@ -4,34 +4,44 @@ declare(strict_types=1);
 
 namespace HyperfTest\Cases;
 
-use Hyperf\Database\Model\Factory;
 use HyperfTest\HttpTestCase;
-use PHPUnit\Framework\TestCase;
 
 class ClientTest extends HttpTestCase
 {
     
-    public function testGetSuccess(): void
+    public function testGetSuccessWithNoData(): void
     {
-        Factory(App\Model\Client::class,10);
-        $this->assertTrue(true);
+
+        $data = $this->client->get('/clients');
+        
+        $this->assertSame([],$data);
     }
 
     public function testGetByIdSuccess(): void
     {
-        $this->assertTrue(true);
+        $data = $this->client->get('/clients/1000000');
+        
+        $this->assertSame(null,$data);
     }
 
     public function testSaveSuccess(): void
     {
-        $this->assertTrue(true);
+        $data = $this->client->post('/clients',[
+            'name'=>'Mercado Livre'
+        ]);
+        
+        $this->assertSame(null,$data);
     }
     public function testDeleteSuccess(): void
     {
-        $this->assertTrue(true);
+        $data = $this->client->delete('/clients',[]);
+        
+        $this->assertSame(null,$data);
     }
     public function testGetFreightsSuccess(): void
     {
-        $this->assertTrue(true);
+        $data = $this->client->get('/clients/freight/1000000',[]);
+        
+        $this->assertSame(null,$data);
     }
 }
