7785f25d59595575329caeed3a6fdceaade7bad7
Guilherme S. Zulian <guilhermesz873@gmail.com>
2023-02-16 01:48:11 -0300

    fix: vendor fixo

--- test/Cases/ClientTest.php
+++ test/Cases/ClientTest.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace HyperfTest\Cases;
 
+use Hyperf\Database\Model\Factory;
 use HyperfTest\HttpTestCase;
 use PHPUnit\Framework\TestCase;
 
-class ClientTest extends TestCase
+class ClientTest extends HttpTestCase
 {
-    public function setUp(): void
-    {
-
-    }
+    
     public function testGetSuccess(): void
     {
+        Factory(App\Model\Client::class,10);
         $this->assertTrue(true);
     }
 
---
+++ vendor/hyperf/guzzle/src/ClientFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Hyperf\Guzzle;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use Hyperf\Utils\Coroutine;
+use Psr\Container\ContainerInterface;
+
+class ClientFactory
+{
+    public function __construct(private ContainerInterface $container)
+    {
+    }
+
+    public function create(array $options = []): Client
+    {
+        $stack = null;
+        if (Coroutine::inCoroutine()) {
+            $stack = HandlerStack::create(new CoroutineHandler());
+        }
+
+        $config = array_replace(['handler' => $stack], $options);
+
+        if (method_exists($this->container, 'make')) {
+            // Create by DI for AOP.
+            return $this->container->make(Client::class, ['config' => $config]);
+        }
+        return new Client($config);
+    }
+}
